c3523dc71be70ac149dd4a2ca512194d5da0b9cc
carloseduu66 <carloseduardogurgel2016@gmail.com>
2025-03-31 19:42:35 -0300

    Conversor Wons para Reais

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function conversor() {
     let valor = prompt("Digite um valor em Wons:");
     
-    // Converte o valor para Reais
     let reais = valor * 0.0039;
     
-    // Formata o valor em Reais com separação de milhar e vírgula como separador decimal
     let reaisFormatado = reais.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 
     alert("O valor em Reais é: " + reaisFormatado);
